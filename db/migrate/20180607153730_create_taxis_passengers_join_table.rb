cafac2fe01a8830f874b390a01dd70409a31fc0c
Chris Cardle <cdcardle@gmail.com>
2018-06-07 15:46:02 +0000

    automatically backed up by learn

--- db/migrate/20180607153730_create_taxis_passengers_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateTaxisPassengersJoinTable < ActiveRecord::Migration
-  def change
-    create_join_table :taxis, :passengers do |t|
-      t.index :taxi_id
-      t.index :passenger_id
-    end
-  end
-end
